d8d2ddcc7d06783a78f874710a9daa66d24bbf99
Gokul-T-M <gokulmanoharan2805@gmail.com>
2025-06-04 08:09:35 +0530

    Some Folder Restructuring

---
+++ CaseStudy_C#/CaseStudySolution/CaseStudy5/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CaseStudy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student(1, "Ravi"));
+            students.Add(new Student(2, "Karthick"));
+            students.Add(new Student(3, "Dhoni"));
+
+            Console.WriteLine("All students :");
+            foreach (Student student in students)
+            {
+                
+                Console.WriteLine($"Id : {student.id} , Name : {student.Name}");
+            }
+
+            Console.WriteLine("Enter name to search");
+            string sname = Console.ReadLine();
+            int c = 0;
+            foreach (Student student in students)
+            {
+                if (student.Name == sname)
+                {
+                    Console.WriteLine($"Found : Id : {student.id} , Name : {student.Name}");
+                    c++;
+                }
+            }
+            if (c == 0)
+            {
+                Console.WriteLine("Student not found");
+            }
+            Console.WriteLine("Enter name to delete:");
+            string sn = Console.ReadLine();
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Name.Equals(sn, StringComparison.OrdinalIgnoreCase))
+                {
+                    students.RemoveAt(i);
+                    Console.WriteLine("Student removed");
+                }
+            }
+            Console.WriteLine("Updated list of students :");
+            foreach (Student student in students)
+            {
+                
+                Console.WriteLine($"Id : {student.id} , Name : {student.Name}");
+            }
+
+            Console.WriteLine("Total number of students : " + students.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CaseStudy_C#/CaseStudySolution/CaseStudy5/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy5
+{
+    class Student
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public Student() { }
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.Name = name;
+        }
+    }
+}
