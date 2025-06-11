d827a660717f88eb835169d322b8b1fa9276e983
Gokul-T-M <gokulmanoharan2805@gmail.com>
2025-06-11 08:55:46 +0530

    Nunit case study added

---
+++ CaseStudy_Nunit/NunitStudent/StudentTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StudentTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
